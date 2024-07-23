19f7f828a6f04af86a6d3767fd507b23c48fc91f
Bash06 <eryk.sarelo@icloud.com>
2024-07-23 16:31:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -21,19 +21,14 @@ import (
 	"github.com/nfnt/resize"
 )
 
-const (
-	chunksize = 1024 //todo
-	offset    = 10   //todo
-
-)
-
 var (
-	allowedScaleValues = []string{"1/1", "1/2", "1/4", "1/8", "1/12", "1/16", "1/32"}
+	allowedScaleValues = []string{"1/1", "1/2", "1/4", "1/8", "1/12", "1/16", "1/32", "1/64"}
 
-	pathPtr    = flag.String("path", "", "Path to the image to be analyzed.")
-	offsetPtr  = flag.Int("offset", 10, "Offset used for RGB colors to decrease similar colors appearing.")
-	scalingPtr = flag.String("scaling", "1/4", "Image scaling used to speed up the program a bit. Available options: 1/1, 1/2, 1/4, 1/8, 1/12, 1/16, 1/32")
-	threadsPtr = flag.Int("threads", 20, "Amount of threads to be created by the process.")
+	pathPtr         = flag.String("path", "", "Path to the image to be analyzed.")
+	scalingPtr      = flag.String("scaling", "1/8", "Image scaling used to speed up the program a bit. Available options: 1/1, 1/2, 1/4, 1/8, 1/12, 1/16, 1/32, 1/64")
+	threadsPtr      = flag.Int("threads", 20, "Amount of threads to be created by the process.")
+	returnAmountPtr = flag.Int("return-amount", 5, "Amount of colors to be returned.")
+	debugPtr        = flag.Bool("debug", false, "Should additional data useful for debugging be shown?")
 )
 
 type KeyValue struct {
@@ -52,7 +47,7 @@ func loadImageAsBuffer(path *string) *bytes.Buffer {
 
 	reader := bufio.NewReader(data)
 	buffer := bytes.NewBuffer(make([]byte, 0))
-	part := make([]byte, chunksize)
+	part := make([]byte, 1024)
 
 	var count int
 
@@ -88,26 +83,11 @@ func checkIfOutOfBounds(x int, y int, bounds image.Rectangle) (bool, bool) {
 	return xOut, yOut
 }
 
-func main() {
-	flag.Parse()
-
-	if !slices.Contains(allowedScaleValues, *scalingPtr) {
-		panic("Invalid scaling provided. Expected one of: 1/1, 1/2, 1/4, 1/8, 1/12, 1/16, 1/32")
-	}
-
-	start := time.Now()
-
-	imgBuffer := loadImageAsBuffer(pathPtr)
-	img, _, err := image.Decode(bytes.NewReader(imgBuffer.Bytes()))
-
-	if err != nil {
-		panic(err)
-	}
-
+func scaleCoordinates(img image.Image) (uint, uint, error) {
 	scaling, err := strconv.Atoi(strings.Split(*scalingPtr, "/")[1])
 
 	if err != nil {
-		panic(err)
+		return 0, 0, err
 	}
 
 	scaledX := img.Bounds().Max.X
@@ -118,19 +98,46 @@ func main() {
 		scaledY = img.Bounds().Max.Y / (scaling / 2)
 	}
 
-	tempImage := resize.Resize(uint(scaledX), uint(scaledY), img, resize.Bilinear)
+	return uint(scaledX), uint(scaledY), nil
+}
+
+func main() {
+	flag.Parse()
+
+	if !slices.Contains(allowedScaleValues, *scalingPtr) {
+		panic("Invalid scaling provided. Expected one of: 1/1, 1/2, 1/4, 1/8, 1/12, 1/16, 1/32, 1/64")
+	}
+
+	isDebug := *debugPtr
+	start := time.Now()
+
+	imgBuffer := loadImageAsBuffer(pathPtr)
+	img, _, err := image.Decode(bytes.NewReader(imgBuffer.Bytes()))
+
+	if err != nil {
+		panic(err)
+	}
+
+	scaledX, scaledY, err := scaleCoordinates(img)
+
+	if err != nil {
+		panic(err)
+	}
+
+	tempImage := resize.Resize(scaledX, scaledY, img, resize.Bilinear)
 	img = tempImage
 
 	pixelCount := img.Bounds().Max.X * img.Bounds().Max.Y
 	colorsMap := make(map[string]int)
 
-	fmt.Println("Iterating over", pixelCount, "pixels")
-
 	var wg sync.WaitGroup
 	var mutex sync.Mutex
 	chunkSize := pixelCount / *threadsPtr
 
-	fmt.Println("Chunk size:", chunkSize)
+	if isDebug {
+		fmt.Println("Iterating over", pixelCount, "pixels")
+		fmt.Println("Chunk size:", chunkSize)
+	}
 
 	for w := 0; w < *threadsPtr; w++ {
 		wg.Add(1)
@@ -138,7 +145,8 @@ func main() {
 		go func(workerID, startPixel, endPixel int) {
 			defer wg.Done()
 			localColorsMap := make(map[string]int)
-			x, y := startPixel%img.Bounds().Max.X, startPixel/img.Bounds().Max.X
+			x := startPixel % img.Bounds().Max.X
+			y := startPixel / img.Bounds().Max.X
 
 			for i := startPixel; i < endPixel; i++ {
 				xOut, yOut := checkIfOutOfBounds(x, y, img.Bounds())
@@ -153,7 +161,7 @@ func main() {
 				}
 
 				rgba := color.RGBAModel.Convert(img.At(x, y)).(color.RGBA)
-				toString := fmt.Sprintf("%d,%d,%d", rgba.R, rgba.G, rgba.B)
+				toString := fmt.Sprintf("%d;%d;%d", rgba.R, rgba.G, rgba.B)
 
 				localColorsMap[toString]++
 				x++
@@ -179,11 +187,21 @@ func main() {
 		return keyValuePairs[i].Value > keyValuePairs[j].Value
 	})
 
-	for i := 0; i < 10; i++ {
-		fmt.Println("RGB:", keyValuePairs[i].Key, ":", keyValuePairs[i].Value, "times")
+	for i := 0; i < *returnAmountPtr; i++ {
+		if i >= len(keyValuePairs) {
+			break
+		}
+
+		rgb := strings.Split(keyValuePairs[i].Key, ";")
+		r, g, b := rgb[0], rgb[1], rgb[2]
+
+		formatString := fmt.Sprintf("[\033[38;2;%s;%s;%sm███\033[0;00m] %s,%s,%s -> Seen %d times", r, g, b, r, g, b, keyValuePairs[i].Value)
+
+		fmt.Println(formatString)
 	}
 
-	duration := time.Since(start)
-
-	fmt.Println("Code took ", duration)
+	if isDebug {
+		duration := time.Since(start)
+		fmt.Println("Code took", duration)
+	}
 }
